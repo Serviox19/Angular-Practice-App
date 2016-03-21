3951f88463a96d26b63688537cd518e7c909fbf7
Servio <servio19@gmail.com>
2016-03-20 20:34:00 -0400

    set up structure

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var PORT = process.env.PORT || 3000;
+
+//mongoose connect
+var db = 'mongodb://localhost/twitter';
+mongoose.connect(db);
+
+//define models
+
+
+app.use(logger('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+
+
+
+
+app.listen(PORT, function(req, res){
+  console.log('App listening on PORT: ' + PORT);
+});
